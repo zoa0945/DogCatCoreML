569b347f805f2b57e51bd44454aab3eef802215e
zoa0945 <zoa0945@gmail.com>
2022-10-18 03:15:38 +0900

    feat: add UI, data and classifier

---
+++ DogCatCoreML/MainViewController.swift
@@ -0,0 +1,86 @@
+//
+//  MainViewController.swift
+//  DogCatCoreML
+//
+//  Created by zoa0945 on 2022/10/17.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .gray
+        image.contentMode = .scaleAspectFill
+        
+        return image
+    }()
+    
+    private let toolBar: UIToolbar = {
+        let toolBar = UIToolbar()
+        let barButtonItem = UIBarButtonItem(systemItem: .camera)
+        let flexibleSpace = UIBarButtonItem(systemItem: .flexibleSpace)
+        toolBar.setItems([barButtonItem, flexibleSpace], animated: true)
+        
+        return toolBar
+    }()
+    
+    private let visualEffectView: UIVisualEffectView = {
+        let visualView = UIVisualEffectView()
+        visualView.contentMode = .scaleToFill
+        visualView.backgroundColor = .white
+        
+        return visualView
+    }()
+    
+    private let contentLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.text = "label"
+        
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layout()
+    }
+}
+
+extension MainViewController {
+    private func layout() {
+        [
+            imageView,
+            visualEffectView,
+            toolBar
+        ].forEach {
+            view.addSubview($0)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        visualEffectView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16)
+            $0.trailing.lessThanOrEqualTo(16)
+            $0.width.equalTo(173)
+            $0.height.equalTo(60)
+        }
+        
+        toolBar.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(visualEffectView.snp.bottom).offset(16)
+        }
+        
+        visualEffectView.contentView.addSubview(contentLabel)
+        
+        contentLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+}
